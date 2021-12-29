2a0e09d32de0c28d30d115b0470690dc2ec26732
secondpaul <30486319+secondpaul@users.noreply.github.com>
2021-12-29 21:21:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,21 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+####################################################################
+## 下载koolProxyR
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+
+####################################################################
+## 下载OpenClash
+wget https://github.com/vernesong/OpenClash/archive/master.zip
+
+## 解压
+unzip master.zip
+
+## 复制OpenClash软件包到OpenWrt
+cp -r OpenClash-master/luci-app-openclash package/
+####################################################################
+
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
